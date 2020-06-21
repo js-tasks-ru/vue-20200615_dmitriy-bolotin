21066b0b7eb22c90338d866c945d1ee44c56e249
Dmitry Bolotin <DBolotin@croc.ru>
2020-06-21 20:36:06 +1000

    task_1-1: add solution

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -1,4 +1,15 @@
 import Vue from '/vendor/vue.esm.browser.js';
 
-// const app = ...
-// Рекомендуется использовать МЕТОД в качестве обработчика события
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    counter: 0,
+  },
+
+  methods: {
+    increment() {
+      return this.counter += 1;
+    }
+  }
+});
