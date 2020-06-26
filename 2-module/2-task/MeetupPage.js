3f20722b6a213679a1009afec78b51cb73d9aa30
Dmitriy Bolotin <DBolotin@croc.ru>
2020-06-26 20:06:56 +1000

    task_2-1: add skeleton for MeetupPage

--- 2-module/2-task/MeetupPage.js
+++ 2-module/2-task/MeetupPage.js
@@ -1,16 +1,70 @@
-// import { MeetupView } from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import { MeetupView } from './MeetupView.js';
+import {
+  MEETUP_ID,
+  fetchMeetup,
+  createIconLink,
+  localeDate,
+  getMeetupCoverLink,
+  agendaItemTitles,
+} from './data.js';
 
-/*export const MeetupPage = {
+export const MeetupPage = {
   name: 'MeetupPage',
 
-  template: `<div>???</div>`,
+  template: `<div><meetup-view></meetup-view></div>`,
 
-  // Components
+  components: {
+    MeetupView,
+  },
 
-  // Data
+  data() {
+    return {
+      isLoading: false,
+      meetupRaw: null,
+    };
+  },
 
-  // Mounted
+  computed: {
+    meetup() {
+      if (!this.meetupRaw) {
+        return;
+      }
 
-  // Methods
-};*/
+      return {
+        ...this.meetupRaw,
+        localeDate: localeDate(this.meetupRaw.date),
+        coverLink: getMeetupCoverLink(this.meetupRaw),
+        normalizedAgenda: this.meetupRaw.agenda.map((agendaItem) => ({
+          ...agendaItem,
+          title: agendaItem.title || agendaItemTitles[agendaItem.type],
+          iconLink: createIconLink(agendaItem),
+          isTalk: agendaItem.type === 'talk',
+        })),
+      };
+    },
+  },
+
+  mounted() {
+    this.loadMeetupRaw();
+  },
+
+  methods: {
+    async loadMeetupRaw() {
+      let meetupRaw;
+
+      this.isLoading = true;
+
+      try {
+        meetupRaw = await fetchMeetup(MEETUP_ID);
+      } catch (error) {
+        this.isLoading = false;
+        console.error(error);
+
+        return;
+      }
+
+      this.meetupRaw = meetupRaw;
+      this.isLoading = false;
+    },
+  },
+};
