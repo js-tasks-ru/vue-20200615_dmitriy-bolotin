ef309538a9fe9e9ad1ff58fda11dfab12f0b5f37
javascriptru <67475495+jsru-1@users.noreply.github.com>
2020-09-06 18:24:54 +0300

    Added teacher solution for task: LoginPage + RegisterPage

--- 4-module/4-task/data.js
+++ 4-module/4-task/data.js
@@ -7,7 +7,15 @@ export const API_URL = 'https://course-vue.javascript.ru/api';
  * @param {string} password - пароль пользователя
  * @return {Promise} - объект с данными пользователя
  */
-export async function login(email, password) {}
+export async function login(email, password) {
+  return fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => res.json());
+}
 
 /**
  * Выполняет авторизацию по логину и паролю и возвращает результат
@@ -16,4 +24,12 @@ export async function login(email, password) {}
  * @param {string} password - пароль пользователя
  * @return {Promise} - объект с данными зарегистрированного пользователя
  */
-export async function register(email, fullname, password) {}
+export async function register(email, fullname, password) {
+  return fetch(`${API_URL}/auth/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, fullname, password }),
+  }).then((res) => res.json());
+}
