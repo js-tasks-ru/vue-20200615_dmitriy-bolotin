48c1bdc9fcb7edb485aee1c6d202f4c4818e5cb1
Dmitry Bolotin <DBolotin@croc.ru>
2020-09-06 22:32:12 +1000

    task_4-5: add solution

--- 4-module/5-task/components/BaseLayout.vue
+++ 4-module/5-task/components/BaseLayout.vue
@@ -1,13 +1,35 @@
-<template></template>
+<template>
+  <div id="app">
+    <div
+      class="progress"
+      style="width: 75%; background-color: rgb(76, 107, 182); opacity: 1;"
+    ></div>
+
+    <div class="wrapper bg-grey">
+      <TheHeader />
+
+      <main class="main">
+        <slot />
+      </main>
+
+      <TheFooter />
+    </div>
+  </div>
+</template>
 
 <script>
+import TheHeader from './TheHeader';
+import TheFooter from './TheFooter';
+
 export default {
   name: 'BaseLayout',
+
+  components: { TheHeader, TheFooter },
 };
 </script>
 
 <style scoped>
-main {
-  flex: 1 0 auto;
-}
+  .main {
+    flex: 1 0 auto;
+  }
 </style>
